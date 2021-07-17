ce660787c7f0687b77128d6c6f3e513648c65947
chen edri <chenedri22@gmail.com>
2021-07-17 20:19:13 +0300

    seconds

--- models/product.py
+++ models/product.py
@@ -7,9 +7,12 @@ class Product(models.Model):
     _description = 'Product'
 
     name = fields.Char('Name')
-    product = fields.Char('Product')
-    category = fields.Char('Category')
+    product_category = fields.Id(String='Product')
     unit_price = fields.Integer(String='Unit Price')
+
+    #if I can add colums to the field I will add :
+    # is_available = fields.Bool(Stirng="availability")
+    # total_amount = field.Integer(String="")
     
 
   
