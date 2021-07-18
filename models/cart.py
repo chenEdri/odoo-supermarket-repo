81495a5e643db985dfcb48e6c5a9ffaab53b6170
chen edri <chenedri22@gmail.com>
2021-07-18 07:06:27 +0300

    fifth

--- models/cart.py
+++ models/cart.py
@@ -11,8 +11,9 @@ class Cart(models.Model):
     # there is only one cart for each customer but a single customer can have more than one cart.
     customer = fields.One2many(comodel_name='Customer', inverse_name='Name')
     # not neccesary- it is a default one
-    date_of_creation = fields.Datetime('YYYY-MM-DD HH:MM:SS')
-    checkout_date = fields.Date('YYYY-MM-DD')
+    creation_time = fields.Datetime('YYYY-MM-DD HH:MM:SS')
+    checkout_time = fields.Datetime('YYYY-MM-DD HH:MM:SS')
+    update_time = fields.Datetime('YYYY-MM-DD HH:MM:SS')
     total_purchase_this_week = fields.Integer(
         compute='_compute_total_purchase')
     is_twice_aweek = fields.Boolean(compute='_compute_twice_aweek')
@@ -21,10 +22,10 @@ class Cart(models.Model):
     def _compute_total_purchase(self):
         return self.product.unit_price * self.quantity
 
-    @api.depends('customer', 'checkout_date')
+    @api.depends('customer', 'checkout_time')
     def _compute_twice_aweek(self):
-        last_check = self.checkout_date
+        last_check = self.checkout_time
         # start_delta = datetime.timedelta(days=last_check[-2:], weeks=1)
         for record in self:
-            if record.checkout_date - last_check > 7 : return False
-        return True  
+            if (record.checkout_date - last_check < 8 ): return True 
+        return False  
